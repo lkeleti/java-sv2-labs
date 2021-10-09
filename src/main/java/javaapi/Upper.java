825fb93dbaa354b2d79537424032c290e5b968c7
lkeleti <keleti.kaszlo@vipmail.hu>
2021-10-09 08:32:36 +0200

    Commit javaapi

--- src/main/java/javaapi/Upper.java
+++ src/main/java/javaapi/Upper.java
@@ -3,6 +3,6 @@ package javaapi;
 public class Upper {
     public static void main(String[] args) {
         System.out.println("Hello World!".toUpperCase());
-        //.toUpperCase() nagybetűsítás
+        //.toUpperCase() nagybetűsítés
     }
 }
