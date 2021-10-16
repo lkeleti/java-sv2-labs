c6bcc9ef9017320bcfd6efd0bb2f7fc092c3c9cc
lkeleti <keleti.kaszlo@vipmail.hu>
2021-10-16 21:48:46 +0200

    commit typeconversion.Conversion

---
+++ training-solutions/src/main/java/typeconversion/Conversion.java
@@ -0,0 +1,27 @@
+package typeconversion;
+
+public class Conversion {
+    public double convertDoubleToDouble(double number) {
+        return ((int) number);
+    }
+
+    public byte[] convertIntArrayToByteArray(int[] intArray) {
+        byte[] byteArray = new byte[intArray.length];
+        for (int i = 0; i < intArray.length; i++) {
+            if (intArray[i] < 0 || intArray[i] > 127) {
+                byteArray[i] = -1;
+            }
+            else {
+                byteArray[i] = (byte) intArray[i];
+            }
+        }
+        return byteArray;
+    }
+
+    public int getFirstDecimal(double number) {
+        int intNumber = (int)number;
+        double result = (number - intNumber) * 10;
+        return (int) result;
+    }
+
+}
---
+++ training-solutions/src/main/java/typeconversion/ConversionMain.java
@@ -0,0 +1,14 @@
+package typeconversion;
+
+import java.util.Arrays;
+
+public class ConversionMain {
+    public static void main(String[] args) {
+        Conversion conversion = new Conversion();
+        System.out.println(conversion.convertDoubleToDouble(345.678));
+
+        System.out.println(Arrays.toString(conversion.convertIntArrayToByteArray(new int[]{5, -14, 6, 2, 125, 354, 9738, 3})));
+
+        System.out.println(conversion.getFirstDecimal(132.567));
+    }
+}
