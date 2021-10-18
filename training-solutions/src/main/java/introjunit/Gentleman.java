2945c90646c502fc234d71d5ce6f5619de4ee88b
lkeleti <keleti.kaszlo@vipmail.hu>
2021-10-18 22:16:17 +0200

    JUnit4 testGentleman

---
+++ training-solutions/src/main/java/introjunit/Gentleman.java
@@ -0,0 +1,13 @@
+package introjunit;
+
+public class Gentleman {
+    public String sayHello(String name) {
+        if (name == null) {
+            return "Hello Anonymous!";
+        }
+        else {
+            return  "Hello " + name +  "!";
+        }
+
+    }
+}
---
+++ training-solutions/src/test/java/introjunit/GentlemanTest.java
@@ -0,0 +1,26 @@
+package introjunit;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class GentlemanTest {
+    @Test
+    public void testGentleman(){
+        //Given
+        Gentleman gentleman = new Gentleman();
+
+        //When
+        String greeting = gentleman.sayHello("John Doe");
+
+        //Then
+        assertThat(greeting, equalTo("Hello John Doe!"));
+
+        //When
+        greeting = gentleman.sayHello(null);
+        //Then
+        assertThat(greeting, equalTo("Hello Anonymous!"));
+    }
+
+}
