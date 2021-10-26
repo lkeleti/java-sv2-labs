89de312dd49f0a407ab81453987c93aeb2e07e3d
lkeleti <keleti.kaszlo@vipmail.hu>
2021-10-26 18:53:02 +0200

    algorithmstransformation done

--- training-solutions/src/main/java/algorithmsdecision/Prime.java
+++ training-solutions/src/main/java/algorithmsdecision/Prime.java
@@ -5,7 +5,7 @@ public class Prime {
         if (number < 2) {
             return false;
         }
-        for (int i = 2; i < number/2; i++){
+        for (int i = 2; i < number/2+1; i++){
             if (number % i == 0){
                 return false;
             }
