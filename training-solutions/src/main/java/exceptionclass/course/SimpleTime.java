df6d4ea48141d6e11bac6048e112d532c4acf0ff
lkeleti <keleti.kaszlo@vipmail.hu>
2021-12-23 08:39:53 +0100

    exceptionsclass exercises completed

--- training-solutions/src/main/java/exceptionclass/course/SimpleTime.java
+++ training-solutions/src/main/java/exceptionclass/course/SimpleTime.java
@@ -5,22 +5,38 @@ public class SimpleTime {
     private int minute;
 
     public SimpleTime(int hour, int minute) {
+        checkHour(hour);
+        checkMinute(minute);
         this.hour = hour;
         this.minute = minute;
     }
 
     public SimpleTime(String time) {
         if (time == null) {
-            throw new InvalideTimeException("Time is null");
+            throw new InvalidTimeException("Time is null");
         }
 
-        if (time.matches("^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$")) {
-            throw new InvalideTimeException("Time is not hh:mm");
+        if (!time.matches("^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$")) {
+            throw new InvalidTimeException("Time is not hh:mm");
         }
+        hour = Integer.parseInt(time.substring(0,2));
+        minute = Integer.parseInt(time.substring(3,5));
+        checkHour(hour);
+        checkMinute(minute);
     }
 
     private boolean checkHour(int hour) {
-        return false;
+        if (hour <  0 || hour > 23 ){
+            throw new InvalidTimeException("Hour is invalid (0-23)");
+        }
+        return true;
+    }
+
+    private boolean checkMinute(int minute) {
+        if (minute <  0 || minute > 59 ){
+            throw new InvalidTimeException("Minute is invalid (0-59)");
+        }
+        return true;
     }
 
     public int getHour() {
@@ -35,7 +51,6 @@ public class SimpleTime {
     public String toString() {
         String h = "0" + hour;
         String m = "0" + minute;
-        return h.substring(h.length()-2,h.length()) + ":" + m.substring(m.length()-2,m.length()) +
-                '}';
+        return h.substring(h.length()-2,h.length()) + ":" + m.substring(m.length()-2,m.length());
     }
 }
