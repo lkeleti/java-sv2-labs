c978527787fe93de7a0601a5695a5521a5992f0d
lkeleti <keleti.laszlo@vipmail.hu>
2022-01-25 09:55:50 +0100

    lambdaintermediate exercises completed

--- training-solutions/src/main/java/lambdaintermediate/Cafe.java
+++ training-solutions/src/main/java/lambdaintermediate/Cafe.java
@@ -2,8 +2,9 @@ package lambdaintermediate;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Cafe {
 
@@ -14,22 +15,43 @@ public class Cafe {
     }
 
     public List<Coffee> getOrderedCoffees() {
-
+        return coffeeOrders.stream()
+                .flatMap(o->o.getCoffees().stream())
+                .collect(Collectors.toList());
     }
 
     public Integer getTotalIncome() {
-
+        return coffeeOrders.stream()
+                .flatMap(o->o.getCoffees().stream())
+                .mapToInt(Coffee::getPrice)
+                .sum();
     }
     public Integer getTotalIncome(LocalDate date) {
-
+        return coffeeOrders.stream()
+                .filter(o->o.getDateTime().toLocalDate().equals(date))
+                .flatMap(o->o.getCoffees().stream())
+                .mapToInt(Coffee::getPrice)
+                .sum();
     }
     public long getNumberOfCoffee(CoffeeType type) {
-
+        return coffeeOrders.stream()
+                .flatMap(o->o.getCoffees().stream())
+                .filter(c->c.getType().equals(type))
+                .mapToInt(Coffee::getPrice)
+                .count();
     }
+
     public List<CoffeeOrder> getOrdersAfter(LocalDateTime from) {
-
+        return coffeeOrders.stream()
+                .filter(o->o.getDateTime().isAfter(from))
+                .collect(Collectors.toList());
     }
-    public List<CoffeeOrder> getFirstFiveOrder(LocalDate date) {
 
+    public List<CoffeeOrder> getFirstFiveOrder(LocalDate date) {
+        return coffeeOrders.stream()
+                .filter(o->o.getDateTime().toLocalDate().equals(date))
+                .sorted(Comparator.comparing(CoffeeOrder::getDateTime))
+                .limit(5)
+                .collect(Collectors.toList());
     }
 }
