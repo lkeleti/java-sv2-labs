4d99a30fae01c2b1e93595f96bb18ac0f8abf2e2
lkeleti <keleti.kaszlo@vipmail.hu>
2022-01-25 08:33:34 +0100

    lambdaintermediate exercises init

--- training-solutions/src/main/java/lambdaintermediate/CoffeeOrder.java
+++ training-solutions/src/main/java/lambdaintermediate/CoffeeOrder.java
@@ -3,18 +3,23 @@ package lambdaintermediate;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
 public class CoffeeOrder {
-    private List<Coffee> orderedCoffees;
+    private List<Coffee> coffees;
     private LocalDateTime dateTime;
 
-    public CoffeeOrder(List<Coffee> orderedCoffees, LocalDateTime dateTime) {
-        this.orderedCoffees = orderedCoffees;
+    public CoffeeOrder(List<Coffee> coffees, LocalDateTime dateTime) {
+        this.coffees = coffees;
         this.dateTime = dateTime;
     }
     public LocalDateTime getDateTime() {
         return dateTime;
+    }
+
+    public List<Coffee> getCoffees() {
+        return new ArrayList<>(coffees);
     }
 }
